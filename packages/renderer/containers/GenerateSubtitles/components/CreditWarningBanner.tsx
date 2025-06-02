b64e42666633fb749c751da9fc99b6b8fc1756ef
mikey1384 <mikey1384@gmail.com>
2025-06-02 20:32:52 +0700

    minor fix

--- packages/renderer/containers/GenerateSubtitles/components/CreditWarningBanner.tsx
+++ packages/renderer/containers/GenerateSubtitles/components/CreditWarningBanner.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Trans } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { Alert } from '../../../components/design-system/index.js';
 
 interface CreditWarningBannerProps {
@@ -9,10 +9,17 @@ interface CreditWarningBannerProps {
 export default function CreditWarningBanner({
   onSettingsClick,
 }: CreditWarningBannerProps) {
+  const { t } = useTranslation();
+
   return (
     <Alert variant="warning">
       <Trans
         i18nKey="generateSubtitles.creditWarning.message"
+        values={{
+          settingsLinkText: t(
+            'generateSubtitles.creditWarning.settingsLinkText'
+          ),
+        }}
         components={{
           settingsLink: (
             <a
