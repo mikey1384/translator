ed6a0d018e8c839dda633c3874ca3c9f106dc79e
mikey1384 <mikey1384@gmail.com>
2025-06-13 07:53:41 +0700

    Revert "fix logo color not appearing correctly on windows"
    This reverts commit 5eea98e4b7625fbb35ab17278150435621eac30c.

--- packages/renderer/components/LogoDisplay.tsx
+++ packages/renderer/components/LogoDisplay.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/css';
-import { colors } from '../styles';
+
+const logoTextColor = '#FAF0E6';
 
 const logoContainerStyles = css`
   display: flex;
@@ -11,7 +12,7 @@ const translatorTextStyles = css`
   font-family: 'Montserrat', 'Nunito Sans', 'Poppins', sans-serif;
   font-weight: 700;
   font-size: 1.1rem;
-  color: ${colors.dark};
+  color: ${logoTextColor};
   margin: 0;
   line-height: 1.1;
   text-align: center;
@@ -21,7 +22,7 @@ const bylineTextStyles = css`
   font-family: 'Montserrat', 'Nunito Sans', 'Poppins', sans-serif;
   font-weight: 500;
   font-size: 0.7rem;
-  color: ${colors.dark};
+  color: ${logoTextColor};
   margin: 0;
   margin-top: 2px;
   line-height: 1;
