7d2cf986d22954332fd4d5b25a12398af267042f
mikey1384 <mikey1384@gmail.com>
2026-08-23 17:42:39 +0700

    Update distribution manifests for v1.16.25

--- distribution/homebrew/stage5-translator.rb
+++ distribution/homebrew/stage5-translator.rb
@@ -1,9 +1,9 @@
 cask "stage5-translator" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.16.6"
-  sha256 arm:   "34d83a142b1355a94412a8961cbe7b1d1bd00a442d57282d85b1c042154451ae",
-         intel: "c243928db00297e380242b45ba4709cc3878d88d3b28582cfbf881e2f8b5e0f4"
+  version "1.16.25"
+  sha256 arm:   "8155cbd75918b30641bc28dac38288ba41794ed2eebe646de3bc7a14919ab965",
+         intel: "169d04102d7ce013686240c1ec1afc503240a699ac256b22ace40d8428533a0d"
 
   url "https://github.com/mikey1384/translator/releases/download/v#{version}/Translator-#{version}-darwin-#{arch}.zip",
       verified: "github.com/mikey1384/translator/"
